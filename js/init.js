6f4e2123f5722a2abd681f996973c5972eb5950b
Sou1Jkeee <1nfern0@list.ru>
2020-04-24 19:11:51 +0300

    initial commit

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(document).ready(function(){
+  $('.carousel').carousel({
+    fullwidth: true,
+    indicators: true
+  });
+});
